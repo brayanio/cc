34ec556c8ecfcd5ee6e2e32f438bc4f9ab55ea3b
Brayan Byrdsong <brayanbyrdsong@gmail.com>
2020-05-16 00:53:12 +0000

    preparing for boss

---
+++ server/data/boss.js
@@ -0,0 +1,25 @@
+// utils
+const Data = (name, plural, map, ...vals) => {
+    let exp = {}
+    exp[plural] = vals
+    exp[name] = {}
+    vals.forEach(e => map(exp[name], e))
+    return exp
+  }
+  const nospace = str => str.split(' ').join('')
+  
+  // data
+  const monster = (name, level, speed, hp, damageDie, attack, loot, abilities, ...tags) => {
+    return {name, level, speed, hp, damageDie, attack, loot, abilities, tags}
+  }
+  
+  let MonsterData = Data('Monster', 'Monsters', (data, e) => data[nospace(e.name)] = e,
+    monster('Lich', 3, 75, 3, 4, 8, {}, [], 'undead'), // boss
+  )
+  
+  /*
+  Title.HelloWorld === 'Hello World'
+  Titles[0] === 'Hello World'
+  */
+  
+  module.exports = {...MonsterData}
